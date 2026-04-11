f8652d28d39fa0d16e72c8e5d5c5d9788aa8139e
felipe1496 <fodasilva@protonmail.com>
2026-04-11 14:31:53 -0300

    centralize domain types and implement api injection in queries

--- src/queries/recurrences-queries.ts
+++ src/queries/recurrences-queries.ts
@@ -1,14 +1,23 @@
-import { RecurrencesService } from '../services/RecurrencesService';
 import type { QueryOpts } from '../utils/types';
+import type { Api } from '../api/api';
+
+type ListRecurrencesFn = Api<unknown>['recurrences']['listRecurrences'];
+export type ListRecurrencesResponse = NonNullable<Awaited<ReturnType<ListRecurrencesFn>>['data']>;
+export type Recurrence = NonNullable<
+  NonNullable<ListRecurrencesResponse['data']>['recurrences']
+>[number];
 
 export const recurrencesKeys = {
   all: () => ['recurrences'] as const,
   getRecurrences: (queryOpts?: QueryOpts) => [...recurrencesKeys.all(), { queryOpts }] as const,
 };
 
-export function getRecurrencesQueryOpts(queryOpts?: QueryOpts) {
+export function getRecurrencesQueryOpts(api: Api<unknown>, queryOpts?: QueryOpts) {
   return {
     queryKey: recurrencesKeys.getRecurrences(queryOpts),
-    queryFn: () => RecurrencesService.getRecurrences(queryOpts),
+    queryFn: () =>
+      api.recurrences
+        .listRecurrences(queryOpts as Parameters<ListRecurrencesFn>[0])
+        .then((res) => res.data),
   };
 }
