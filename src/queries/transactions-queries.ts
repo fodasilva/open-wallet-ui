6ccec6d05306f9b585ae0e045003c02c078fd465
SonyNeto <sonyneto@gmail.com>
2026-04-25 20:23:29 -0300

    refactor: update uses of the deprecated createFilter function for string manipulation

--- src/queries/transactions-queries.ts
+++ src/queries/transactions-queries.ts
@@ -5,8 +5,6 @@ type ListEntriesFn = Api<unknown>['transactions']['v1ListEntries'];
 export type ListEntriesResponse = NonNullable<Awaited<ReturnType<ListEntriesFn>>['data']>;
 export type Entry = NonNullable<NonNullable<ListEntriesResponse['data']>['entries']>[number];
 
-type GetSummaryFn = Api<unknown>['transactions']['v1GetSummary'];
-
 export const transactionsKeys = {
   all: () => ['transactions'] as const,
   getEntries: (queryOpts?: QueryOpts) => [...transactionsKeys.all(), { queryOpts }] as const,
@@ -16,19 +14,13 @@ export const transactionsKeys = {
 export function getEntriesQueryOpts(api: Api<unknown>, queryOpts?: QueryOpts) {
   return {
     queryKey: [...transactionsKeys.getEntries(queryOpts)],
-    queryFn: () =>
-      api.transactions
-        .v1ListEntries(queryOpts as Parameters<ListEntriesFn>[0])
-        .then((res) => res.data),
+    queryFn: () => api.transactions.v1ListEntries(queryOpts).then((res) => res.data),
   };
 }
 
 export function getSummaryQueryOpts(api: Api<unknown>, filter: string) {
   return {
     queryKey: transactionsKeys.getSummary(filter),
-    queryFn: () =>
-      api.transactions
-        .v1GetSummary({ filter } as Parameters<GetSummaryFn>[0])
-        .then((res) => res.data),
+    queryFn: () => api.transactions.v1GetSummary({ filter }).then((res) => res.data),
   };
 }
