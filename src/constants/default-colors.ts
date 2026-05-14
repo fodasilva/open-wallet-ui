2b9d06adaf63037556a1e470531803ed7fc0eb90
felipe1496 <fodasilva@protonmail.com>
2026-05-13 23:52:41 -0300

    commit

--- src/constants/default-colors.ts
+++ src/constants/default-colors.ts
@@ -1,12 +1,12 @@
-export const DEFAULT_COLORS = {
-  RED: '#fb2c36',
-  ORANGE: '#ff6900',
-  YELLOW: '#efb100',
-  LIME: '#7ccf00',
-  GREEN: '#00c951',
-  CYAN: '#00bba7',
-  BLUE: '#2b7fff',
-  PURPLE: '#ad46ff',
-  PINK: '#ec4899',
-  ROSE: '#f43f5e',
-};
+export const DEFAULT_COLORS = [
+  '#fb2c36',
+  '#ff6900',
+  '#efb100',
+  '#7ccf00',
+  '#00c951',
+  '#00bba7',
+  '#2b7fff',
+  '#ad46ff',
+  '#ec4899',
+  '#f43f5e',
+];
